c2edef4bf0d1e8be52825521da8e58105e56b7bc
maxmekiska <maxmekiska@gmail.com>
2022-07-03 01:22:17 +0100

    solving import md read problem

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
+import os
 
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     author="Maximilian Mekiska",
