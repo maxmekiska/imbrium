089ae3076384e6becc69effc28db89e9b6fb1dc3
maxmekiska <maxmekiska@gmail.com>
2022-07-03 01:15:50 +0100

    markdown long description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     author="Maximilian Mekiska",
     author_email="maxmekiska@gmail.com",
     url="https://github.com/maxmekiska/Imbrium",
     description="Standard and Hybrid Deep Learning Multivariate-Multi-Step & Univariate-Multi-Step Time Series Forecasting.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     name="Imbrium",
     version="0.1.0",
     packages = find_packages(include=["imbrium", "imbrium.*"]),
